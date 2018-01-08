951d570b05c38a528350c4501d3704f0b9420667
matt-komm <Matthias.Komm@cern.ch>
2018-01-08 22:42:20 +0000

    add example training

---
+++ training.py
@@ -0,0 +1,368 @@
+import tensorflow as tf
+import keras
+from keras import backend as K
+import os
+import time
+from root_reader import root_reader
+
+from keras.layers import Dense, Dropout, Flatten,Convolution2D, Convolution1D,LSTM,Concatenate
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+
+def block_deepFlavourConvolutions(charged,neutrals,vertices,dropoutRate,active=True,batchnorm=False,batchmomentum=0.6):
+    '''
+    deep Flavour convolution part. 
+    '''
+    cpf=charged
+    if active:
+        cpf  = Convolution1D(64, 1, kernel_initializer='lecun_uniform',  activation='relu', name='cpf_conv0')(cpf)
+        if batchnorm:
+            cpf = BatchNormalization(momentum=batchmomentum ,name='cpf_batchnorm0')(cpf)
+        cpf = Dropout(dropoutRate,name='cpf_dropout0')(cpf)                                                   
+        cpf  = Convolution1D(32, 1, kernel_initializer='lecun_uniform',  activation='relu', name='cpf_conv1')(cpf)
+        if batchnorm:
+            cpf = BatchNormalization(momentum=batchmomentum,name='cpf_batchnorm1')(cpf)
+        cpf = Dropout(dropoutRate,name='cpf_dropout1')(cpf)                                                   
+        cpf  = Convolution1D(32, 1, kernel_initializer='lecun_uniform',  activation='relu', name='cpf_conv2')(cpf)
+        if batchnorm:
+            cpf = BatchNormalization(momentum=batchmomentum,name='cpf_batchnorm2')(cpf)
+        cpf = Dropout(dropoutRate,name='cpf_dropout2')(cpf)                                                   
+        cpf  = Convolution1D(8, 1, kernel_initializer='lecun_uniform',  activation='relu' , name='cpf_conv3')(cpf)
+    else:
+        cpf = Convolution1D(1,1, kernel_initializer='zeros',trainable=False)(cpf)
+        
+    npf=neutrals
+    if active:
+        npf = Convolution1D(32, 1, kernel_initializer='lecun_uniform',  activation='relu', name='npf_conv0')(npf)
+        if batchnorm:
+            npf = BatchNormalization(momentum=batchmomentum,name='npf_batchnorm0')(npf)
+        npf = Dropout(dropoutRate,name='npf_dropout0')(npf) 
+        npf = Convolution1D(16, 1, kernel_initializer='lecun_uniform',  activation='relu', name='npf_conv1')(npf)
+        if batchnorm:
+            npf = BatchNormalization(momentum=batchmomentum,name='npf_batchnorm1')(npf)
+        npf = Dropout(dropoutRate,name='npf_dropout1')(npf)
+        npf = Convolution1D(4, 1, kernel_initializer='lecun_uniform',  activation='relu' , name='npf_conv2')(npf)
+    else:
+        npf = Convolution1D(1,1, kernel_initializer='zeros',trainable=False)(npf)
+
+    vtx = vertices
+    if active:
+        vtx = Convolution1D(64, 1, kernel_initializer='lecun_uniform',  activation='relu', name='vtx_conv0')(vtx)
+        if batchnorm:
+            vtx = BatchNormalization(momentum=batchmomentum,name='vtx_batchnorm0')(vtx)
+        vtx = Dropout(dropoutRate,name='vtx_dropout0')(vtx) 
+        vtx = Convolution1D(32, 1, kernel_initializer='lecun_uniform',  activation='relu', name='vtx_conv1')(vtx)
+        if batchnorm:
+            vtx = BatchNormalization(momentum=batchmomentum,name='vtx_batchnorm1')(vtx)
+        vtx = Dropout(dropoutRate,name='vtx_dropout1')(vtx)
+        vtx = Convolution1D(32, 1, kernel_initializer='lecun_uniform',  activation='relu', name='vtx_conv2')(vtx)
+        if batchnorm:
+            vtx = BatchNormalization(momentum=batchmomentum,name='vtx_batchnorm2')(vtx)
+        vtx = Dropout(dropoutRate,name='vtx_dropout2')(vtx)
+        vtx = Convolution1D(8, 1, kernel_initializer='lecun_uniform',  activation='relu', name='vtx_conv3')(vtx)
+    else:
+        vtx = Convolution1D(1,1, kernel_initializer='zeros',trainable=False)(vtx)
+
+    return cpf,npf,vtx
+
+def block_deepFlavourDense(x,dropoutRate,active=True,batchnorm=False,batchmomentum=0.6):
+    if active:
+        x=  Dense(200, activation='relu',kernel_initializer='lecun_uniform', name='df_dense0')(x)
+        if batchnorm:
+            x = BatchNormalization(momentum=batchmomentum,name='df_dense_batchnorm0')(x)
+        x = Dropout(dropoutRate,name='df_dense_dropout0')(x)
+        x=  Dense(100, activation='relu',kernel_initializer='lecun_uniform', name='df_dense1')(x)
+        if batchnorm:
+            x = BatchNormalization(momentum=batchmomentum,name='df_dense_batchnorm1')(x)
+        x = Dropout(dropoutRate,name='df_dense_dropout1')(x)
+        x=  Dense(100, activation='relu',kernel_initializer='lecun_uniform', name='df_dense2')(x)
+        if batchnorm:
+            x = BatchNormalization(momentum=batchmomentum,name='df_dense_batchnorm2')(x)
+        x = Dropout(dropoutRate,name='df_dense_dropout2')(x)
+        x=  Dense(100, activation='relu',kernel_initializer='lecun_uniform', name='df_dense3')(x)
+        if batchnorm:
+            x = BatchNormalization(momentum=batchmomentum,name='df_dense_batchnorm3')(x)
+        x = Dropout(dropoutRate,name='df_dense_dropout3')(x)
+        x=  Dense(100, activation='relu',kernel_initializer='lecun_uniform', name='df_dense4')(x)
+        if batchnorm:
+            x = BatchNormalization(momentum=batchmomentum,name='df_dense_batchnorm4')(x)
+        x = Dropout(dropoutRate,name='df_dense_dropout4')(x)
+        x=  Dense(100, activation='relu',kernel_initializer='lecun_uniform', name='df_dense5')(x)
+        if batchnorm:
+            x = BatchNormalization(momentum=batchmomentum,name='df_dense_batchnorm5')(x)
+        x = Dropout(dropoutRate,name='df_dense_dropout5')(x)
+        x=  Dense(100, activation='relu',kernel_initializer='lecun_uniform', name='df_dense6')(x)
+        if batchnorm:
+            x = BatchNormalization(momentum=batchmomentum,name='df_dense_batchnorm6')(x)
+        x = Dropout(dropoutRate,name='df_dense_dropout6')(x)
+        x=  Dense(100, activation='relu',kernel_initializer='lecun_uniform', name='df_dense7')(x)
+        if batchnorm:
+            x = BatchNormalization(momentum=batchmomentum,name='df_dense_batchnorm7')(x)
+        x = Dropout(dropoutRate,name='df_dense_dropout7')(x)
+    else:
+        x= Dense(1,kernel_initializer='zeros',trainable=False,name='df_dense_off')(x)
+    
+    return x
+
+def model_deepFlavourReference(Inputs,nclasses,nregclasses,dropoutRate=0.1,momentum=0.6):
+    #deep flavor w/o pt regression
+    
+    globalvars = BatchNormalization(momentum=momentum,name='globals_input_batchnorm') (Inputs[0])
+    cpf    =     BatchNormalization(momentum=momentum,name='cpf_input_batchnorm')     (Inputs[1])
+    npf    =     BatchNormalization(momentum=momentum,name='npf_input_batchnorm')     (Inputs[2])
+    vtx    =     BatchNormalization(momentum=momentum,name='vtx_input_batchnorm')     (Inputs[3])
+    #ptreginput = BatchNormalization(momentum=momentum,name='reg_input_batchnorm')     (Inputs[4])
+
+    cpf,npf,vtx = block_deepFlavourConvolutions(charged=cpf,
+                                                neutrals=npf,
+                                                vertices=vtx,
+                                                dropoutRate=dropoutRate,
+                                                active=True,
+                                                batchnorm=True, batchmomentum=momentum)
+
+
+    #
+    cpf  = LSTM(150,go_backwards=True,implementation=2, name='cpf_lstm')(cpf)
+    cpf=BatchNormalization(momentum=momentum,name='cpflstm_batchnorm')(cpf)
+    cpf = Dropout(dropoutRate)(cpf)
+
+    npf = LSTM(50,go_backwards=True,implementation=2, name='npf_lstm')(npf)
+    npf=BatchNormalization(momentum=momentum,name='npflstm_batchnorm')(npf)
+    npf = Dropout(dropoutRate)(npf)
+
+    vtx = LSTM(50,go_backwards=True,implementation=2, name='vtx_lstm')(vtx)
+    vtx=BatchNormalization(momentum=momentum,name='vtxlstm_batchnorm')(vtx)
+    vtx = Dropout(dropoutRate)(vtx)
+
+
+    x = Concatenate()( [globalvars,cpf,npf,vtx ])
+
+    x = block_deepFlavourDense(x,dropoutRate,active=True,batchnorm=True,batchmomentum=momentum)
+
+    flavour_pred=Dense(nclasses, activation='softmax',kernel_initializer='lecun_uniform',name='ID_pred')(x)
+
+    #reg = Concatenate()( [flavour_pred, ptreginput ] ) 
+
+    #reg_pred=Dense(nregclasses, activation='linear',kernel_initializer='ones',name='regression_pred',trainable=True)(reg)
+
+    #predictions = [flavour_pred,reg_pred]
+    #model = Model(inputs=Inputs, outputs=predictions)
+
+    return flavour_pred
+    
+    
+
+
+fileList = []
+
+filePath = "/media/matthias/HDD/matthias/Analysis/LLP/training/samples/rootFiles.raw.txt"
+#filePath = "/vols/cms/mkomm/LLP/samples/rootFiles.txt"
+
+f = open(filePath)
+for l in f:
+    absPath = os.path.join(filePath.rsplit('/',1)[0],l.replace("\n","").replace("\r","")+"")
+    fileList.append(absPath)
+f.close()
+print len(fileList)
+
+fileList = fileList[:4]
+
+print fileList
+
+featureDict = {
+
+     "sv" : {
+        "branches":[
+            'sv_pt',
+            'sv_deltaR',
+            'sv_mass',
+            'sv_ntracks',
+            'sv_chi2',
+            'sv_normchi2',
+            'sv_dxy',
+            'sv_dxysig',
+            'sv_d3d',
+            'sv_d3dsig',
+            'sv_costhetasvpv',
+            'sv_enratio',
+            
+        ],
+        "multiplicity":"n_sv",
+        "max":4
+    },
+
+    "truth": {
+        "branches":[
+            'isB/UInt_t',
+            'isBB/UInt_t',
+            'isGBB/UInt_t',
+            'isLeptonicB/UInt_t',
+            'isLeptonicB_C/UInt_t',
+            'isC/UInt_t',
+            'isCC/UInt_t',
+            'isGCC/UInt_t',
+            'isUD/UInt_t',
+            'isS/UInt_t',
+            'isG/UInt_t',
+            'isUndefined/UInt_t',
+            'isFromLLgno_isB/UInt_t',
+            'isFromLLgno_isBB/UInt_t',
+            'isFromLLgno_isGBB/UInt_t',
+            'isFromLLgno_isLeptonicB/UInt_t',
+            'isFromLLgno_isLeptonicB_C/UInt_t',
+            'isFromLLgno_isC/UInt_t',
+            'isFromLLgno_isCC/UInt_t',
+            'isFromLLgno_isGCC/UInt_t',
+            'isFromLLgno_isUD/UInt_t',
+            'isFromLLgno_isS/UInt_t',
+            'isFromLLgno_isG/UInt_t',
+            'isFromLLgno_isUndefined/UInt_t'
+        ],
+        "multiplicity":None
+    },
+    "global": {
+        "branches": [
+            'jet_pt',
+            'jet_eta',
+            'nCpfcand',
+            'nNpfcand',
+            'nsv',
+            'npv',
+            'TagVarCSV_trackSumJetEtRatio', 
+            'TagVarCSV_trackSumJetDeltaR', 
+            'TagVarCSV_vertexCategory', 
+            'TagVarCSV_trackSip2dValAboveCharm', 
+            'TagVarCSV_trackSip2dSigAboveCharm', 
+            'TagVarCSV_trackSip3dValAboveCharm', 
+            'TagVarCSV_trackSip3dSigAboveCharm', 
+            'TagVarCSV_jetNSelectedTracks', 
+            'TagVarCSV_jetNTracksEtaRel'
+        ],
+        "multiplicity":None
+
+    },
+
+
+    "Cpfcan": {
+        "branches": [
+            'Cpfcan_BtagPf_trackEtaRel',
+            'Cpfcan_BtagPf_trackPtRel',
+            'Cpfcan_BtagPf_trackPPar',
+            'Cpfcan_BtagPf_trackDeltaR',
+            'Cpfcan_BtagPf_trackPParRatio',
+            'Cpfcan_BtagPf_trackSip2dVal',
+            'Cpfcan_BtagPf_trackSip2dSig',
+            'Cpfcan_BtagPf_trackSip3dVal',
+            'Cpfcan_BtagPf_trackSip3dSig',
+            'Cpfcan_BtagPf_trackJetDistVal',
+
+            'Cpfcan_ptrel', 
+            'Cpfcan_drminsv',
+            'Cpfcan_VTX_ass',
+            'Cpfcan_puppiw',
+            'Cpfcan_chi2',
+            'Cpfcan_quality'
+        ],
+        "multiplicity":"n_Cpfcand",
+        "max":25
+    },
+    "Npfcan": {
+        "branches": [
+            'Npfcan_ptrel',
+            'Npfcan_deltaR',
+            'Npfcan_isGamma',
+            'Npfcan_HadFrac',
+            'Npfcan_drminsv',
+            'Npfcan_puppiw'
+        ],
+        "multiplicity":"n_Npfcand",
+        "max":25
+    }
+}
+
+loss_mean = []
+
+for epoch in range(10):
+    print "epoch",epoch+1
+    fileListQueue = tf.train.string_input_producer(fileList, num_epochs=1, shuffle=True)
+
+    rootreader_op = [
+        root_reader(fileListQueue, featureDict,batch=100).batch() for _ in range(4)
+    ]
+    
+    batchSize = 10000
+    minAfterDequeue = batchSize*2
+    capacity = minAfterDequeue + 3*batchSize
+    
+    #check: tf.contrib.training.stratified_sample
+    #for online resampling for equal pt/eta weights
+    #trainingBatch = tf.train.batch_join(
+    trainingBatch = tf.train.shuffle_batch_join(
+        rootreader_op, 
+        batch_size=batchSize, 
+        capacity=capacity,
+        min_after_dequeue=minAfterDequeue,
+        enqueue_many=True #requires to read examples in batches!
+    )
+
+    globalvars = keras.layers.Input(tensor=trainingBatch['global'])
+    cpf = keras.layers.Input(tensor=trainingBatch['Cpfcan'])
+    npf = keras.layers.Input(tensor=trainingBatch['Npfcan'])
+    vtx = keras.layers.Input(tensor=trainingBatch['sv'])
+    truth = trainingBatch["truth"]
+    #dequeueBatch = trainingBatch['Npfcan'].dequeue()
+
+    nclasses = truth.shape.as_list()[1]
+    print nclasses
+    inputs = [globalvars,cpf,npf,vtx]
+    prediction = model_deepFlavourReference(inputs,nclasses,1,dropoutRate=0.1,momentum=0.6)
+    loss = tf.reduce_mean(keras.losses.categorical_crossentropy(truth, prediction))
+    model = keras.Model(inputs=inputs, outputs=prediction)
+    if os.path.exists("model_epoch"+str(epoch-1)+".hdf5"):
+        print "loading weights ..."
+        model.load_weights("model_epoch"+str(epoch-1)+".hdf5")
+    #model.add_loss(loss)
+    #model.compile(optimizer='rmsprop', loss=None)
+    #model.summary()
+    train_op = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+
+
+
+    #init_op = tf.global_variables_initializer() #bug https://github.com/tensorflow/tensorflow/issues/1045
+    init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+
+    sess = K.get_session()
+
+    sess.run(init_op)
+
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+    total_loss = 0
+    try:
+        step = 0
+        while not coord.should_stop():
+            start_time = time.time()
+
+            _, loss_value = sess.run([train_op, loss], feed_dict={K.learning_phase(): 0})
+            total_loss+=loss_value
+            
+            #data = sess.run(trainingBatch)
+            #print data
+            duration = time.time() - start_time
+            if step % 1 == 0:
+                print 'Step %d: loss = %.2f (%.3f sec)' % (step, loss_value,duration)
+            step += 1
+    except tf.errors.OutOfRangeError:
+        print('Done training for %d steps.' % (step))
+    model.save_weights("model_epoch"+str(epoch)+".hdf5")
+    loss_mean.append(total_loss/step)
+    coord.request_stop()
+    coord.join(threads)
+    K.clear_session()
+        
+for i,l in enumerate(total_loss):
+    print i+1,l
+    
+    
+    
+    
